2adb3ce64b1abf69a218b92d4f2e9738a0dd98ce
Rostyslav` <hrostyslav@ukr.net`>
2026-05-18 13:45:45 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+if (tree) {
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const parentLi = e.target.closest('li');
+      const list = parentLi ? parentLi.querySelector('ul') : null ;
+
+      if (list) {
+        if (list.style.display === 'none') {
+          list.style.display = '';
+        } else {
+          list.style.display = 'none';
+        }
+      }
+    }
+  });
+}
